91aea8268fdef8435da75c485ac022578e26846b
ccastano46 <castanocamilo522@gmail.com>
2023-10-03 19:33:36 -0500

    Listo el pollo

--- ProyectosPython/setup.py
+++
@@ -1,7 +0,0 @@
-from setuptools import setup, find_packages
-setup(
-    name= 'domain',
-    extras_require=dict(test=['pytest']),
-    packages=find_packages(where='src'),
-    package_dir={"": "src"},
-)
